502a4739ceaf90fdb5126c6e966756d132920a91
Angelo Schuler Piletti <74366173+AngeloSchulerPiletti@users.noreply.github.com>
2021-08-24 14:02:38 -0300

    changing ajax to async function. NO MORE CONFLICTS!

--- js/dynamicPageComponents.js
+++ js/dynamicPageComponents.js
@@ -1,25 +1,64 @@
-function loadPageX(page_index) {
-    let ajax = new XMLHttpRequest();
-    ajax.open("GET", "html/pages/page"+page_index+".html", false);
-    ajax.send();
-    document.querySelector('main').innerHTML = ajax.responseText;
+function makeRequest(page_index) {
+    return new Promise(function (resolve, reject) {
+        let xhr = new XMLHttpRequest();
+        xhr.open("GET", "html/pages/page" + page_index + ".html");
+        xhr.onload = function () {
+            if (this.status >= 200 && this.status < 300) {
+                resolve(xhr.response);
+                // resolve('you got it!')
+            } else {
+                reject({
+                    status: this.status,
+                    statusText: xhr.statusText
+                });
+            }
+        };
+        xhr.onerror = function () {
+            reject({
+                status: this.status,
+                statusText: xhr.statusText
+            });
+        };
+        xhr.send();
+    });
+}
+async function loadPageX(page_index) {
+    makeRequest(page_index).then(value =>{
+        document.querySelector('main').innerHTML = value;
+        settingPageXMetaElements(page_index);
+        settingPageXTitle(page_index);
+        showingTargetPage();
+    });
 }
 
 
-function settingPageXMetaElements(page_index){
-    let pageMetaPlace = document.querySelector('.page_meta'),
-        script = document.createElement('script'),
+
+
+function settingPageXMetaElements(page_index) {
+    var pageMetaPlace = document.querySelector('.page_meta');
+    let script = document.createElement('script'),
         style = document.createElement('link');
 
-    script.setAttribute('src', 'js/per_page/page'+page_index+'.js');
+    script.setAttribute('src', 'js/per_page/page' + page_index + '.js');
     script.setAttribute('type', 'module');
     style.setAttribute('rel', 'stylesheet');
-    style.setAttribute('href', 'css/per_page/page'+page_index+'.css');
+    style.setAttribute('href', 'css/per_page/page' + page_index + '.css');
     pageMetaPlace.appendChild(style);
     pageMetaPlace.appendChild(script);
 
-    if (page_index == 0){
+    if (page_index == 0) {
         setParticles(pageMetaPlace);
+
+        let script = document.createElement('script'),
+            style = document.createElement('link');
+
+        script.setAttribute('src', 'js/script/carousel.js');
+        script.setAttribute('type', 'module');
+        style.setAttribute('rel', 'stylesheet');
+        style.setAttribute('href', 'css/per_page/specific_features/carousel.css');
+        pageMetaPlace.appendChild(style);
+        pageMetaPlace.appendChild(script);
+
     }
 }
 
@@ -32,8 +71,8 @@ var titles = {
     'page2': "Page 2",
     'page3': "Page 3",
 };
-function settingPageXTitle(page_index){
-    document.querySelector('title').innerHTML = titles['page'+page_index];
+function settingPageXTitle(page_index) {
+    document.querySelector('title').innerHTML = titles['page' + page_index];
 }
 
 
@@ -46,14 +85,13 @@ function showingTargetPage() {
 
 
 function setParticles(placeToInsert) {
-    var app_js = document.createElement('script'),
-        particles_js = document.createElement('script');
-    
-    app_js.setAttribute('src', '/js/script/particles-js/app.js');
+    var particles_js = document.createElement('script');
+
+    // app_js.setAttribute('src', '/js/script/particles-js/app.js');
     particles_js.setAttribute('src', '/js/script/particles-js/particles.js');
 
     placeToInsert.appendChild(particles_js);
-    placeToInsert.appendChild(app_js);
+    // placeToInsert.appendChild(app_js);
 }
 
 
@@ -62,10 +100,9 @@ function setParticles(placeToInsert) {
 /*+-------------------------------------------+
   |                 FIRST LOAD                |
   +-------------------------------------------+ */
-loadPageX(0);
-settingPageXMetaElements(0);
-settingPageXTitle(0);
-showingTargetPage();
+window.addEventListener('DOMContentLoaded', function(){
+    loadPageX(0);
+})
 
 
 /*+-------------------------------------------+
@@ -74,10 +111,7 @@ showingTargetPage();
 var links = document.querySelectorAll('.nav_link');
 
 for (let index = 0; index < links.length; index++) {
-    links[index].addEventListener('click', function(){
+    links[index].addEventListener('click', function () {
         loadPageX(index);
-        settingPageXMetaElements(index);
-        settingPageXTitle(index);
-        showingTargetPage();
     });
 }
