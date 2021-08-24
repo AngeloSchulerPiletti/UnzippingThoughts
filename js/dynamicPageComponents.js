9ad547fd623e6d6e36ac70b5b78090fe56f975eb
Angelo Schuler Piletti <74366173+AngeloSchulerPiletti@users.noreply.github.com>
2021-08-24 14:32:03 -0300

    saving code lines and turning code clear

--- js/dynamicPageComponents.js
+++ js/dynamicPageComponents.js
@@ -1,3 +1,4 @@
+/* ======================= REQUEST PROMISE ==================== */
 function makeRequest(page_index) {
     return new Promise(function (resolve, reject) {
         let xhr = new XMLHttpRequest();
@@ -30,69 +31,80 @@ async function loadPageX(page_index) {
         showingTargetPage();
     });
 }
+/* ======================= REQUEST PROMISE ==================== */
 
 
 
-
+/* ======================= SETTING METADATA =================== */
 function settingPageXMetaElements(page_index) {
     var pageMetaPlace = document.querySelector('.page_meta');
-    let script = document.createElement('script'),
-        style = document.createElement('link');
 
-    script.setAttribute('src', 'js/per_page/page' + page_index + '.js');
-    script.setAttribute('type', 'module');
-    style.setAttribute('rel', 'stylesheet');
-    style.setAttribute('href', 'css/per_page/page' + page_index + '.css');
-    pageMetaPlace.appendChild(style);
-    pageMetaPlace.appendChild(script);
+    setStyle(pageMetaPlace, 'per_page/page' + page_index + '.css');
+    setScript(pageMetaPlace, 'js/per_page/page' + page_index + '.js', 'module');
 
-    if (page_index == 0) {
-        setParticles(pageMetaPlace);
-
-        let script = document.createElement('script'),
-            style = document.createElement('link');
-
-        script.setAttribute('src', 'js/script/carousel.js');
-        script.setAttribute('type', 'module');
-        style.setAttribute('rel', 'stylesheet');
-        style.setAttribute('href', 'css/per_page/specific_features/carousel.css');
-        pageMetaPlace.appendChild(style);
-        pageMetaPlace.appendChild(script);
-
+    switch (page_index) {
+        case 0:
+            setScript(pageMetaPlace, 'script/particles-js/particles.js');
+            setScript(pageMetaPlace, 'script/carousel.js');
+            break;
+        default:
+            break;
     }
 }
+/* ======================= SETTING METADATA =================== */
 
 
 
 
-var titles = {
-    'page0': "UnzippingThoughts",
-    'page1': "Page 1",
-    'page2': "Page 2",
-    'page3': "Page 3",
-};
+/* ======================= TITLE PAGE CONFG ==================== */
+var titles = [
+    "UnzippingThoughts",
+    "Page 1",
+    "Page 2",
+    "Page 3",
+];
 function settingPageXTitle(page_index) {
-    document.querySelector('title').innerHTML = titles['page' + page_index];
+    document.querySelector('title').innerHTML = titles[page_index];
 }
+/* ======================= TITLE PAGE CONFG ==================== */
 
 
 
+
+/* ========================= PAGE FADE IN ====================== */
 function showingTargetPage() {
     setTimeout(() => {
         document.querySelector('.page_content').dataset.anim = "on";
     }, 100);
 }
+/* ========================= PAGE FADE IN ====================== */
 
 
-function setParticles(placeToInsert) {
-    var particles_js = document.createElement('script');
 
-    // app_js.setAttribute('src', '/js/script/particles-js/app.js');
-    particles_js.setAttribute('src', '/js/script/particles-js/particles.js');
 
-    placeToInsert.appendChild(particles_js);
-    // placeToInsert.appendChild(app_js);
+/* ===================== USEFUL SETTING FUNCS ================== */
+function setScript(placeToInsert, src, type="application/javascript") {
+    var script_js = document.createElement('script');
+    script_js.setAttribute('src', '/js/'+src);
+    script_js.setAttribute('type', type);
+    placeToInsert.appendChild(script_js);
 }
+function setStyle(placeToInsert, src, rel="stylesheet") {
+    var style_css = document.createElement('link');
+    style_css.setAttribute('href', '/css/'+src);
+    style_css.setAttribute('rel', rel);
+    placeToInsert.appendChild(style_css);
+}
+/* ===================== USEFUL SETTING FUNCS ================== */
+
+
+
+
+
+
+
+
+
 
 
 
