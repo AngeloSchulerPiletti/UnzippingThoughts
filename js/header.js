dead10ad305f03644cff661197e25603b7c5a7a9
Angelo Schuler Piletti <74366173+AngeloSchulerPiletti@users.noreply.github.com>
2021-09-07 19:28:22 -0300

    header fixed reduced

--- js/header.js
+++ js/header.js
@@ -1,10 +1,13 @@
 var header = document.querySelector('header');
+var logo = header.querySelector('#header_logo');
 
 window.addEventListener('scroll', function(){
     if (self.pageYOffset > header.offsetHeight && header.dataset.move != "on"){
+        logo.style.display = "none";
         header.dataset.move = "on";
     }
     else if(self.pageYOffset == 0 && header.dataset.move == "on"){
+        logo.style.display = "block";
         header.dataset.move = "off"
     }
 
