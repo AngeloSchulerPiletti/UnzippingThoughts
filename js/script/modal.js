dfb0fda8a2468825e561be0c3233d9cd71f0ace9
Angelo Schuler Piletti <74366173+AngeloSchulerPiletti@users.noreply.github.com>
2021-09-08 12:45:21 -0300

    modal created

--- js/script/modal.js
+++ js/script/modal.js
@@ -23,6 +23,24 @@ for (let i = 0; i < keys.length; i++) {
     fetchingText(keys[i]);
 }
 
-export function showModal(modalCode) {
+export function showModal(modalCode){
+    modalContainer.querySelector('h1').innerHTML = modalData[modalCode][0];
+    modalContainer.querySelector('p').innerHTML = modalData[modalCode][1];
     
+    modalContainer.style.display = "block";
+    setTimeout(() => {
+        modalContainer.dataset.show = "on";
+    }, 10);
+    document.body.style.overflow = "hidden";
 }
+function closeModal(){
+    modalContainer.dataset.show = "off";
+    setTimeout(() => {
+        modalContainer.style.display = "none";
+        document.body.style.overflow = "auto";
+    }, 600);
+}
+
+modalContainer.addEventListener('click', event =>{
+        closeModal();
+})
