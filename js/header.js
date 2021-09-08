51b28e50a3369a3efb21939788bd809fc424bd25
Angelo Schuler Piletti <74366173+AngeloSchulerPiletti@users.noreply.github.com>
2021-09-08 13:12:33 -0300

    header fixed when has nothing

--- js/header.js
+++ js/header.js
@@ -1,14 +1,19 @@
 var header = document.querySelector('header');
 var logo = header.querySelector('#header_logo');
 
-window.addEventListener('scroll', function(){
-    if (self.pageYOffset > header.offsetHeight && header.dataset.move != "on"){
-        logo.style.display = "none";
-        header.dataset.move = "on";
+window.addEventListener('scroll', function () {
+    if (header.querySelector('#header_nav').innerHTML != "") {
+        if (self.pageYOffset > header.offsetHeight && header.dataset.move != "on") {
+            logo.style.display = "none";
+            header.dataset.move = "on";
+        }
+        else if (self.pageYOffset == 0 && header.dataset.move == "on") {
+            logo.style.display = "block";
+            header.dataset.move = "off"
+        }
     }
-    else if(self.pageYOffset == 0 && header.dataset.move == "on"){
+    else{
         logo.style.display = "block";
         header.dataset.move = "off"
     }
-
 })
